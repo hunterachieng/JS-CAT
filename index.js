91b20ddd83e7c42094cb89d88329a7b8324dd2e1
hunterachieng <achienghunter@gmail.com>
2021-06-07 19:53:20 +0300

    JS-CAT

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ In this scenario, 'this' does not refer to the global object because it is withi
 is bound to an object which turns its execution context from global to within the scope it has been 
 defined in. 
 When this happens, 'this' now becomes the particular object where in the code, var self = this; 
-this will now become myObject and inherit all its properties unpto its current scope. When invoked, 
+'this' will now become myObject and inherit all its properties upto its current scope. When invoked, 
 outer func prints out "lovelace" when this.name and self.name are used. This is because self is used to refer
-to the object itself. When the property name is invoked in inner func, this.name prints out undefined because
-the outer func was execution context of 'this' has switched back to being a global object as the function's
+to 'this' which is the object itself. When the property name is invoked in inner func, this.name prints out undefined because
+the outer func which was the execution context of 'this' has switched back to being a global object as the function's
 lifecycle has ended. 
 Self.name still prints out "lovelace" within inner func because it is used to refer to the original this
 even though the contexts have changed.*/
